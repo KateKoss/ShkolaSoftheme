cd27f4a9bdd3f04516ec45bb91240763cd0b6ca8
KateKoss <777koska@gmail.com>
2018-07-02 16:36:42 +0300

    Two-way data binding

--- Frontend/ArticlesApp/src/app/app.module.ts
+++ Frontend/ArticlesApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ArticlesListComponent } from './articles-list/article-list.component';
@@ -13,7 +14,8 @@ import { ArticleListService } from './articles-list/article-list.service';
     ArticleDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule  
   ],
   providers: [ArticleListService],
   bootstrap: [AppComponent]
--- Frontend/ArticlesApp/src/app/articles-list/article-details/article-details.component.ts
+++ Frontend/ArticlesApp/src/app/articles-list/article-details/article-details.component.ts
@@ -1,14 +1,20 @@
 import { Component, Input } from '@angular/core';
 
 import { Article } from './article.model'
+import { tick } from '@angular/core/testing';
 
 @Component({
     selector: 'article-details',
     templateUrl: './article-details.component.html',
     styleUrls: ['./article-details.component.css']
 })
-export class ArticleDetailsComponent{
+export class ArticleDetailsComponent{    
     @Input()
-    article : Article
+    article : Article;
+
+    isColorGrey : boolean = false;
+    changeColor​(){
+        this.isColorGrey = !this.isColorGrey;
+    }
 }
 
